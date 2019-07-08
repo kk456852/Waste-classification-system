8eb23f7a8cfbcd1c23e3a9029133e701162cf1e7
Charley Zhao <30900241+CharleyZhao123@users.noreply.github.com>
2019-07-08 15:32:10 +0800

    AI与web第一次连接

--- WCS/classification_AI/model_load_predict.py
+++ WCS/classification_AI/model_load_predict.py
@@ -1,12 +1,11 @@
 import numpy as np
 from keras.models import load_model
-from load_data import load_pic
+from .load_data import load_pic
 
 
-
-def ProcessAPI(pic_path, model_path="model.h5"):
+def ProcessAPI(pic_path,
+               model_path="C:\\Users\chali\Desktop\Waste-classification-system\WCS\classification_AI\model.h5"):
     model = load_model(model_path)
-    data = load_pic(pic_path)
+    data = load_pic("C:\\Users\chali\Desktop\Waste-classification-system\WCS"+pic_path)
     test_result = model.predict([[data]])
     return test_result
-    

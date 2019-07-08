6e851e78296758de06d3e8c774b0a19b5a447b67
kk456852 <36686544+kk456852@users.noreply.github.com>
2019-07-08 15:36:15 +0800

    fix Bug of model

--- WCS/classification_AI/model_load_predict.py
+++ WCS/classification_AI/model_load_predict.py
@@ -4,7 +4,7 @@ from load_data import load_pic
 
 
 
-def ProcessAPI(pic_path, model_path="model.h5"):
+def ProcessAPI(pic_path, model_path="model_final.h5"):
     model = load_model(model_path)
     data = load_pic(pic_path)
     test_result = model.predict([[data]])

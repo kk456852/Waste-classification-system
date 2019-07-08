000125c9c4a6cb29c32581e34ba2f6db6f5b7fd6
kk456852 <36686544+kk456852@users.noreply.github.com>
2019-07-08 14:57:03 +0800

    加载模型预测

--- WCS/classification_AI/model_load_predict.py
+++ WCS/classification_AI/model_load_predict.py
@@ -1,17 +1,12 @@
 import numpy as np
 from keras.models import load_model
-from keras.datasets import  mnist
-MDOEL_PATH = "model.h5"
+from load_data import load_pic
 
 
 
-print("------------Loading_model---------------")
-model = load_model(MDOEL_PATH)
-print("------------Loading_data---------------")
-
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train = x_train.reshape(x_train.shape[0],-1)/255.0
-x_test = x_test.reshape(x_test.shape[0], -1) / 255.0
-
-
-print(model.predict(x_test[:]))
+def ProcessAPI(pic_path, model_path="model.h5"):
+    model = load_model(model_path)
+    data = load_pic(pic_path)
+    test_result = model.predict([[data]])
+    return test_result
+    
